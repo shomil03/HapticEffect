45498e0ed3f09fde88fa18462fb17d10be6325c3
shomil03 <shomil02020@gmail.com>
2024-03-13 08:56:51 +0530

    Initial Commit

---
+++ HapticEffect/HapticEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticEffectApp.swift
+//  HapticEffect
+//
+//  Created by Shomil Singh on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HapticEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
